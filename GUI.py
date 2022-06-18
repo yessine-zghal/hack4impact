19eabc54a1fdf46dcb9f4dcaa45ae28da15e2fa5
Yessine Zghal <74101918+yessine-zghal@users.noreply.github.com>
2022-06-18 22:40:01 +0100

    import dependency
    v1

---
+++ GUI.py
@@ -0,0 +1,13 @@
+from Controller.controleur import *
+from Controller.controleur  import Image,ImageTk
+from tkinter import *
+
+##################### definition ########################
+source = ""
+destination = ""
+t = ""
+font = ("Myriad Arabic",16)
+
+def Traduct(event):
+    return Traduire(txt)
+########################### principal code ##############################################
