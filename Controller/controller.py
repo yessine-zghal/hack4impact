6d6db3aebf724744118b08c0349c2eec992318f9
abir <bahriabir080@gmail.com>
2022-06-19 01:45:24 +0200

    integrate image in platforme
    v2

--- Controller/controller.py
+++ Controller/controller.py
@@ -26,6 +26,9 @@ hands = mpHands.Hands(static_image_mode=False,
 #detection hands from camera
 mpDraw = mp.solutions.drawing_utils
 #------------------------------------------------------------------#
+
+
+
 ######################### commun ###################################
 #design button to click on fir your tikenter interface 
 def createButton(window,text,font,color,command,x,y,w,h,image,texte,box):
@@ -41,4 +44,68 @@ def createButton(window,text,font,color,command,x,y,w,h,image,texte,box):
         buton = Button(window, text=text, font=font, background=color, command=lambda: set_text(texte, command),image=image)
     buton= buton.place(x=x,y=y,width=w,height=h)
     return buton
+#-----------------------------------------------------------------#
+######################### root #####################################
+#declaration root image for your code 
+def declarationimageroot():
+    rootback = Image.open('view/Img/rootback.png')
+    nas = Image.open('view/Img/nas.png')
+    ichara = Image.open('view/Img/ichara.png')
+    rootback = ImageTk.PhotoImage(rootback)
+    nas = ImageTk.PhotoImage(nas)
+    ichara = ImageTk.PhotoImage(ichara)
+    return rootback,ichara,nas
+#-----------------------------------------------------------------#
+
+
+######################### nas ######################################
+#change from ichara to nas
+def testpath(let,box,i,j):
+        result = listeImg(let[i][j])
+        if result != None:
+            res = Image.open(result)
+            imgdet = ImageTk.PhotoImage(res)
+            box.configure(image=imgdet)
+            box.image = imgdet
+            print(result)
+            if(j < len(let[i])-1):
+                j=j+1
+                k=2
+            elif(i<len(let)-1):
+              i=i+1
+              j=0
+              k=6
+            else:
+                return
+            box.after(500*k,testpath,let,box,i,j)
+        return
+#deleting the text printed
+def delete(texte):
+    txte = texte.get("1.0", END)
+    txte = txte[:-2]
+    texte.delete('1.0', END)
+    texte.insert(END,txte)
+#adding text 
+def ajout(texte, box):
+    print("here")
+    liste = []
+    txte = texte.get("1.0", END)
+    liste = txte.split()
+    testpath(liste,box,0,0)
+#declaration the image text
+def declarationimagenas():
+    nasback = Image.open('view/Img/nasback.png')
+    nas_btn = Image.open('view/Img/nas_btn.png')
+    nasback = ImageTk.PhotoImage(nasback)
+    nas_btn = ImageTk.PhotoImage(nas_btn)
+    img2 = ImageTk.PhotoImage(Image.open('Model/data/2a.png'))
+    return nasback,nas_btn,img2
+#labled the image
+def listeImg (val):
+    data_uses={'ا':"2a",'ب':"ba",'ت':"ta",'ث':"tha",'ج':"jeem",'ح':"7a",'خ':"kha",'ر':"ra",'ز':"za",'س':"sa",
+                'ش':"cha",'ص':"sad",'ض':"dha",'ع':"3aa",'غ':"gha",'ف':"fa",'ق':"9a",'ك':"kef",'ط':"taa",'ظ':"dhad",'ه'
+               :"ha", 'ل':"la",'ي':"ya",'ن':"nun",'م':"mim",'د':"da",'ذ':"dhe",'و':"waw"}
+    if(val in data_uses):
+        return os.path.join("Model/data",data_uses[val]+".png")
+    return None
 #-----------------------------------------------------------------#
